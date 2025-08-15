5207d0f37ad720df02275a58005cbcf3fc5a722f
otachami <otachaminjk@co.jp>
2025-08-15 10:57:35 +0900

    演算子実践

--- lib/main.dart
+++ lib/main.dart
@@ -29,9 +29,22 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int result1 = 1 + 2;
+  int result2 = 6 - 2;
+  int result3 = 2 * 3;
+  int result4 = 10 ~/ 3;
+  double result5 = 10 / 3;
+  int result6 = 10 % 3;
+  String title = 'こんにちは。' + '世界！';
+  int result1Plus2 = 0;
+  String title1 = '';
+  String title2 = '';
 
   @override
   Widget build(BuildContext context) {
+    result1Plus2 = result1 + result2;
+    title1 = "result1 = $result1";
+    title2 = "result2 = ${result2}";
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -40,6 +53,18 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('result1 =[$result1]'),
+            Text('result2 =[$result2]'),
+            Text('result3 =[$result3]'),
+            Text('result4 =[$result4]'),
+            Text('result5 =[$result5]'),
+            Text('result6 =[$result6]'),
+            Text('title =[$title]'),
+            Text('resultPlus2 = [$result1Plus2]'),
+            Text('title1 =[$title1]'),
+            Text('title2 =[$title2]'),
+          ],
         ),
       ),
     ); // This trailing comma makes auto-formatting nicer for build methods.
